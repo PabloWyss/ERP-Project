c5c68124c849013c03163850868a6e3de262009a
Pablo Wyss <pabloawyss@gmail.com>
2023-04-13 13:26:09 +0200

    nginx https added

--- backend/item/urls.py
+++ backend/item/urls.py
@@ -1,11 +1,11 @@
-# from django.urls import path
-#
-# from item.views import ListItemView, SearchItemView, RetrieveUpdateDestroyItemView
+from django.urls import path
+
+from item.views import ListItemView, CreateItemView
 
 urlpatterns = [
     # backend/api/items/
-    # path('', ListItemView.as_view()),
-    # path('new/', CreateItemView.as_view()),
+    path('', ListItemView.as_view()),
+    path('new/', CreateItemView.as_view()),
     # path('search/', SearchItemView.as_view()),
     # path('<int:item_id>/', RetrieveUpdateDestroyItemView.as_view())
 ]
