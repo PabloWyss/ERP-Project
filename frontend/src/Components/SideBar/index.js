740bdaccfdc08df0889f71319ca01f7cac693dcf
Hatice Anik <haticeanik12@gmail.com>
2023-04-13 15:58:53 +0200

    SideBar Buttons change color when clicked

--- frontend/src/Components/SideBar/index.js
+++ frontend/src/Components/SideBar/index.js
@@ -5,47 +5,59 @@ import { ReactComponent as WarehouseIcon } from '../../Assets/Icons/warehouse.sv
 import { ReactComponent as ReportsIcon } from '../../Assets/Icons/reports.svg';
 import { ReactComponent as OrdersIcon } from '../../Assets/Icons/orders.svg';
 import { ReactComponent as ItemsIcon } from '../../Assets/Icons/items.svg';
-import { Button } from 'tailwind-react-ui';
+
 
 const SideBar = () => {
   return (
     <div className="fixed left-0 top-0 h-full flex flex-col w-48 justify-between bg-green-500">
       <div className="mt-8">
-        <NavLink to="/profile" activeClassName="bg-blue-500">
-          <Button className="flex items-center w-full text-left">
-            <ProfileIcon className="mr-2" />
-            Profile
-          </Button>
+        <NavLink to="/profile" >
+          {({isActive}) => (
+            <button className={`flex items-center w-full text-left mt-2 ${isActive ? 'bg-blue-500' : ''}`}>
+              <ProfileIcon className="mr-2" />
+              Profile
+            </button>
+          )}
         </NavLink>
-        <NavLink to="/tags" activeClassName="bg-blue-500">
-          <Button className="flex items-center w-full text-left mt-2">
-            <TagsIcon className="mr-2" />
-            Tags
-          </Button>
+        <NavLink to="/tags">
+          {({isActive}) => (
+            <button className={`flex items-center w-full text-left mt-2 ${isActive ? 'bg-blue-500' : ''}`}>
+              <TagsIcon className="mr-2" />
+              Tags
+            </button>
+          )}
         </NavLink>
-        <NavLink to="/warehouse" activeClassName="bg-blue-500">
-          <Button className="flex items-center w-full text-left mt-2">
-            <WarehouseIcon className="mr-2" />
-            Warehouse
-          </Button>
+        <NavLink to="/warehouse" >
+          {({isActive}) => (
+            <button className={`flex items-center w-full text-left mt-2 ${isActive ? 'bg-blue-500' : ''}`}>
+              <WarehouseIcon className="mr-2" />
+              Warehouse
+            </button>
+          )}
         </NavLink>
-        <NavLink to="/reports" activeClassName="bg-blue-500">
-          <Button className="flex items-center w-full text-left mt-2">
-            <ReportsIcon className="mr-2" />
-            Reports
-          </Button>
+        <NavLink to="/reports" >
+          {({isActive}) => (
+            <button className={`flex items-center w-full text-left mt-2 ${isActive ? 'bg-blue-500' : ''}`}>
+              <ReportsIcon className="mr-2" />
+              Reports
+            </button>
+          )}
         </NavLink>
-        <NavLink to="/orders" activeClassName="bg-blue-500">
-          <Button className="flex items-center w-full text-left mt-2">
-            <OrdersIcon className="mr-2" />
-            Orders
-          </Button>
+        <NavLink to="/orders">
+          {({isActive}) => (
+            <button className={`flex items-center w-full text-left mt-2 ${isActive ? 'bg-blue-500' : ''}`}>
+              <OrdersIcon className="mr-2" />
+              Orders
+            </button>
+          )}
         </NavLink>
-        <NavLink to="/items" activeClassName="bg-blue-500">
-          <Button className="flex items-center w-full text-left mt-2">
-            <ItemsIcon className="mr-2" />
-            Items
-          </Button>
+        <NavLink to="/items">
+          {({isActive}) => (
+            <button className={`flex items-center w-full text-left mt-2 ${isActive ? 'bg-blue-500' : ''}`}>
+              <ItemsIcon className="mr-2" />
+              Items
+            </button>
+          )}
         </NavLink>
       </div>
       <div className="h-230px"></div>
@@ -54,4 +66,5 @@ const SideBar = () => {
 };
 
 export default SideBar;
+
 
