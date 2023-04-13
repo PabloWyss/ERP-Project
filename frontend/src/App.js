4b0ee5b0dc67dcd0c12fbd2c382b5531317cda37
Enrico Giangrossi <enrico@giangrossi.de>
2023-04-13 14:07:21 +0200

    Tailwind theme configured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,17 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <p className="text-2xl font-semibold">App Page</p>
+      <p className="text-lg font-semibold">App Page</p>
       <p>App Page</p>
-      <button className="bg-black text-white w-fit p-3">Test Button</button>
+      <p className="text-title">Title</p>
+      <p className="text-section">Title</p>
+      <button className="bg-ifOrange text-white font-medium">Test Button</button>
+      <input type="text" value={'Test'} />
+      <select>
+        <option>Manta</option>
+        <option>Banta</option>
+      </select>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gradientFrom: "#56BB9D",
+        gradientTo: "#39534F",
+        backgroundGrey: "#F0F1F3",
+        textGrey: "#48505E",
+        buttonGrey: "#5D6679",
+        drawGrey: "#D0D3D9",
+        ifOrange: "#F4A261",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"], //"sans" is applied to the whole document by default
+      },
+      borderRadius: {
+        ifRadius: "10px",
+      },
+    },
   },
   plugins: [],
 };
