e9ab1cf5e1ffa7a8943a6c5c269edd0a78a9b44a
Pablo Wyss <pabloawyss@gmail.com>
2023-04-14 08:42:48 +0200

    backend_warehouses_views

--- backend/warehouse/urls.py
+++ backend/warehouse/urls.py
@@ -1,12 +1,11 @@
-# from django.urls import path
-#
-# from warehouse.views import ListWarehouseView, SearchWarehouseView, AssignItemToWarehouseView, ListCreateWarehouseView, \
-#     RetrieveUpdateDestroyWarehouseView
+from django.urls import path
+
+from warehouse.views import ListWarehouseView, CreateWarehouseView
 
 urlpatterns = [
     # backend/api/warehouses/
-    # path('', ListWarehouseView.as_view()),
-    # path('new/', CreateWarehouseView.as_view()),
+    path('', ListWarehouseView.as_view()),
+    path('new/', CreateWarehouseView.as_view()),
     # path('assign/<int:warehouse_id>/', AssignItemToWarehouseView.as_view()),
     # path('search/', SearchWarehouseView.as_view()),
     # path('<int:warehouse_id>/', RetrieveUpdateDestroyWarehouseView.as_view())
