620270d7aa49f7be323d4d289dce3459ad680c15
Enrico Giangrossi <enrico@giangrossi.de>
2023-04-21 13:51:57 +0200

    Choice buy/sell, select supplier.

--- frontend/src/Pages/Orders/NewOrder/index.js
+++ frontend/src/Pages/Orders/NewOrder/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SelectPartner from "./SelectPartner";
 
 function NewOrder() {
   //fake data for table testing
@@ -45,6 +46,23 @@ function NewOrder() {
     },
   ];
 
+  //handle choice buy or sell
+  const [isBuy, setIsBuy] = useState(true);
+  let supplierOrCustomer = "Supplier";
+  const changeOrderType = () => {
+    setIsBuy(!isBuy);
+    supplierOrCustomer = isBuy ? "Supplier" : "Customer";
+    console.log(supplierOrCustomer);
+    //TODO trigger fetching list of suppliers or customers
+  };
+  useEffect(() => {}, [isBuy]);
+
+  //handle choice refund or not
+  const [isRefund, setIsRefund] = useState(false);
+  const changeIsRefund = () => {
+    setIsRefund(!isRefund);
+  };
+
   return (
     <div
       className="h-screen w-screen py-6 px-8 justify-center
@@ -57,7 +75,62 @@ function NewOrder() {
         overflow-y-auto scrollbar-thin scrollbar-track-transparent
         scrollbar-thumb-drawGrey hover:scrollbar-thumb-buttonGrey"
       >
-        <h1 className="text-title mb-2">New Order</h1>
+        <h1 className="text-title mb-4">New Order</h1>
+        <div className="flex gap-8">
+          <div className="flex">
+            <div className="pt-2 mr-2 text-buttonGrey">Type: </div>
+            <button
+              onClick={changeOrderType}
+              className={
+                isBuy
+                  ? "rounded-r-none bg-ifOrange text-white"
+                  : "rounded-r-none border-2 border-ifOrange text-buttonGrey"
+              }
+            >
+              Buy
+            </button>
+            <button
+              onClick={changeOrderType}
+              className={
+                isBuy
+                  ? "rounded-l-none border-2 border-ifOrange text-buttonGrey"
+                  : "rounded-l-none bg-ifOrange text-white"
+              }
+            >
+              Sell
+            </button>
+          </div>
+          <div className="flex">
+            <div className="pt-2 mr-2 text-buttonGrey">Refund: </div>
+            <button
+              onClick={changeIsRefund}
+              className={
+                isRefund
+                  ? "rounded-r-none bg-ifOrange text-white"
+                  : "rounded-r-none border-2 border-ifOrange text-buttonGrey"
+              }
+            >
+              Yes
+            </button>
+            <button
+              onClick={changeIsRefund}
+              className={
+                isRefund
+                  ? "rounded-l-none border-2 border-ifOrange text-buttonGrey"
+                  : "rounded-l-none bg-ifOrange text-white"
+              }
+            >
+              No
+            </button>
+          </div>
+        </div>
+        <h2 className=" bg-backgroundGrey text-section px-4 mt-4">
+          {isBuy ? "Supplier" : "Customer"}
+        </h2>
+        <div className="my-2">
+          {/* it will need to get the data for the table as props */}
+          <SelectPartner />
+        </div>
       </div>
     </div>
   );
