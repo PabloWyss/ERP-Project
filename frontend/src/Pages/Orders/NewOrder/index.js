6cd06fc1abbabd80ce23ca80594666df4c5eda7e
Enrico Giangrossi <enrico@giangrossi.de>
2023-04-21 08:58:01 +0200

    Added route for new order

--- frontend/src/Pages/Orders/NewOrder/index.js
+++ frontend/src/Pages/Orders/NewOrder/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import ListTable from "../../Components/ListTable/ListTable";
 
-function Warehouse() {
+function NewOrder() {
   //fake data for table testing
   const data = [
     {
@@ -19,54 +18,6 @@ function Warehouse() {
       order: "58418278790810",
       partner: "customer",
       id: "2",
-    },
-    {
-      name: "Eric Ferris",
-      address: "906 Hart Country Lane, Toccoa, GA 30577",
-      date: "07/10/2020",
-      order: "81534454080477",
-      partner: "supplier",
-      id: "3",
-    },
-    {
-      name: "Gloria Noble",
-      address: "2403 Edgewood Avenue, Fresno, CA 93721",
-      date: "07/09/2020",
-      order: "20452221703743",
-      partner: "supplier",
-      id: "4",
-    },
-    {
-      name: "Darren Daniels",
-      address: "882 Hide A Way Road, Anaktuvuk Pass, AK 99721",
-      date: "07/07/2020",
-      order: "22906126785176",
-      partner: "customer",
-      id: "5",
-    },
-    {
-      name: "Ted McDonald",
-      address: "796 Bryan Avenue, Minneapolis, MN 55406",
-      date: "07/07/2020",
-      order: "87574505851064",
-      partner: "supplier",
-      id: "6",
-    },
-    {
-      name: "Abra Kebabra",
-      address: "4420 Rua de la Paz, Toledo, NY 10923",
-      date: "07/11/2023",
-      order: "67349585892118",
-      partner: "customer",
-      id: "7",
-    },
-    {
-      name: "Diane Keaton",
-      address: "4420 4th Avenue, Anaheim, WY 10923",
-      date: "05/11/2023",
-      order: "37349585892118",
-      partner: "customer",
-      id: "9",
     },
   ];
 
@@ -106,11 +57,10 @@ function Warehouse() {
         overflow-y-auto scrollbar-thin scrollbar-track-transparent
         scrollbar-thumb-drawGrey hover:scrollbar-thumb-buttonGrey"
       >
-        <h1 className="text-title mb-2">Warehouses</h1>
-        <ListTable data={data} columns={columns}></ListTable>
+        <h1 className="text-title mb-2">New Order</h1>
       </div>
     </div>
   );
 }
 
-export default Warehouse;
+export default NewOrder;
