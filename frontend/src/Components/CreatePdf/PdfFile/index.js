a94e4e66c8a960a3fed60eda26953bfd78895e1c
Pablo Wyss <pabloawyss@gmail.com>
2023-06-08 11:38:51 +0200

    Inputs added to PDF

--- frontend/src/Components/CreatePdf/PdfFile/index.js
+++ frontend/src/Components/CreatePdf/PdfFile/index.js
@@ -2,102 +2,123 @@ import {Document, Page, Text, View, StyleSheet, Image, Svg} from '@react-pdf/ren
 import React, {useState} from "react";
 import QRCode from "qrcode";
 import JsBarcode from "jsbarcode";
+import {v4 as uuidv4} from 'uuid';
 
-// Create styles
-const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-        backgroundColor: '#E4E4E4',
-        flexWrap: 'wrap'
-
-
-    },
-    viewQrcode: {
-        width: "20%",
-        aspectRatio: 1 / 1,
-        border: "1px black solid",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "white",
-    },
-    text: {
-        backgroundColor: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
 
 // Create Document Component
-const MyDocument = ({origin, item, number, selection}) => {
-
-
-        // QRCODE GENERATOR
-        const [qrcode, setQrcode] = useState("")
-        const handleCreateQRCode = () => {
-            const data = {
-                id: item.id,
-                amazon_asin: item.amazon_asin,
-                amazon_fnsku: item.amazon_fnsku,
-                ean: item.ean,
-                sku: item.sku,
-                upc: item.upc,
-            }
-            const datJson = JSON.stringify(data)
-            const generateQR = async text => {
-                try {
-                    setQrcode(await QRCode.toDataURL(datJson))
-                } catch (err) {
-                    console.error(err)
-                }
-            }
-            generateQR()
+const MyDocument = ({origin, item, inputInfo, selection, isStandard}) => {
+    // Create styles
+    const styles = StyleSheet.create({
+        page: {
+            flexDirection: 'row',
+            backgroundColor: '#E4E4E4',
+            flexWrap: 'wrap',
+            paddingTop: `${inputInfo.topMargin}mm`,
+            paddingBottom: `${inputInfo.topMargin}mm`,
+            paddingLeft: `${inputInfo.sideMargin}mm`,
+            paddingRight: `${inputInfo.sideMargin}mm`,
+        }, text: {
+            backgroundColor: "white", display: "flex", alignItems: "center", justifyContent: "center"
+        }, viewStyle: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "white",
+        }, textStyle: {
+            objectFit: "contain", fontSize: "10"
+        }, QRcodeImage: {
+            width: `${inputInfo.width}mm`,
+            border: "1px black solid",
+            aspectRatio: 1 / 1
+        }, BarcodeImage: {
+            height: `${inputInfo.height}mm`,
+            width: `${inputInfo.width}mm`,
+            border: "1px black solid"
         }
 
-        handleCreateQRCode()
 
-        let array = [1]
-        for (let i = 1; i < number; i++) {
+    });
+
+
+    // QRCODE GENERATOR
+    const [qrcode, setQrcode] = useState("")
+    const handleCreateQRCode = () => {
+        const data = {
+            id: item.id,
+            amazon_asin: item.amazon_asin,
+            amazon_fnsku: item.amazon_fnsku,
+            ean: item.ean,
+            sku: item.sku,
+            upc: item.upc,
+        }
+        const datJson = JSON.stringify(data)
+        const generateQR = async text => {
+            try {
+                setQrcode(await QRCode.toDataURL(datJson))
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        generateQR()
+    }
+
+    handleCreateQRCode()
+
+
+    let array = [1]
+    if (isStandard && origin == "Barcode") {
+        inputInfo.quantity = 40
+        for (let i = 1; i < inputInfo.quantity; i++) {
             array.push(i)
         }
-
-        //Barcode Generator
-
-        let canvas;
-        canvas = document.createElement('canvas');
-        if (selection == "EAN") {
-            JsBarcode(canvas, item.ean, {
-                format: "EAN13",
-            });
-        } else {
-            JsBarcode(canvas, item.sku);
+    } else if (isStandard && origin == "QRCode") {
+        inputInfo.quantity = 30
+        for (let i = 1; i < inputInfo.quantity; i++) {
+            array.push(i)
         }
-
-        const barcode = canvas.toDataURL();
-
-
-        return (
-            <Document>
-                <Page size="A4" style={styles.page}>
-                    {origin == "Barcode" ?
-                        array.map((value, index) => {
-                            return <Image style={{height: "29.7mm", width: "42mm", border: "1px black solid"}} key={index}
-                                          src={barcode}/>
-                        }) :
-                        array.map((value, index) => {
-                            return (
-                                <View style={styles.viewQrcode}>
-                                    <Image key={index} src={qrcode}/>
-                                    <Text style={styles.text}>{item.ean}</Text>
-                                </View>
-                            )
-                        })
-                    }
-                </Page>
-            </Document>
-        )
+    } else {
+        for (let i = 1; i < inputInfo.quantity; i++) {
+            array.push(i)
+        }
     }
-;
+
+    //Barcode Generator
+
+    let canvas;
+    canvas = document.createElement('canvas');
+    if (selection == "EAN") {
+        JsBarcode(canvas, item.ean, {
+            format: "EAN13",
+        });
+    } else {
+        JsBarcode(canvas, item.sku);
+    }
+
+    const barcode = canvas.toDataURL();
+
+
+    return (<Document>
+        <Page size="A4" style={styles.page}>
+            {origin == "Barcode" ? array.map((value, index) => {
+
+                return (<View style={styles.viewStyle} key={uuidv4()}>
+                    <Image style={styles.BarcodeImage} key={index}
+                           src={barcode}/>
+                    {
+                        inputInfo.showName ? <Text style={styles.textStyle}>{inputInfo.personalizedText}</Text> : null
+                    }
+                </View>)
+            }) : array.map((value, index) => {
+                return (<View style={styles.viewStyle} key={uuidv4()}>
+                    <Image style={styles.QRcodeImage} key={index} src={qrcode}/>
+                    {
+                        inputInfo.showName ? <Text style={styles.textStyle}>{inputInfo.personalizedText}</Text> : null
+                    }
+                </View>)
+            })}
+        </Page>
+    </Document>)
+};
 
 export default MyDocument
--- frontend/src/Components/CreatePdf/index.js
+++ frontend/src/Components/CreatePdf/index.js
@@ -6,47 +6,156 @@ import {useLocation} from "react-router-dom";
 
 const PDFCreate = () => {
 
-        const [nubmerOf, setNumberOf] = useState(40)
-        const [isStandard, setIsStandard] = useState(true)
-        const location = useLocation()
+    const location = useLocation()
+    const [isStandard, setIsStandard] = useState(true)
+    const [inputIfo, setInputInfo] = useState(
+        {
+            "quantity": 1,
+            "width": "42",
+            "showName": true,
+            "height": "32.7",
+            "topMargin": "0",
+            "sideMargin": "0",
+            "personalizedText": location.state.item.name,
+        })
 
-
-        const handeleInputNumber = (e) => {
-            setNumberOf(e.target.value)
-        }
-        const handeleInputDefault = (e) => {
-            setIsStandard(e.target.checked);
-        }
-
-        return (
-            <div className="flex flex-col w-full">
-                <div className="flex w-1/3 h-1/6 justify-start m-4">
-
-                    <ItemDetailsInput
-                        type={"checkbox"}
-                        disableInput={false}
-                        description={"Standard View"}
-                        value={isStandard}
-                        checked={isStandard}
-                        handleInput={handeleInputDefault}
-                    />
-                    {isStandard ?
-                        "":
-                    <ItemDetailsInput className="flex justify-center w-full"
-                                      value={nubmerOf}
-                                      disableInput={false}
-                                      handleInput={handeleInputNumber}
-                                      description={'Number of Codes'}/>}
-                </div>
-                <PDFViewer className="w-full h-5/6 h-screen">
-                    <MyDocument origin={location.state.origin} item={location.state?.item} number={nubmerOf}
-                                isStandard={isStandard} selection={location.state.selection}/>
-                </PDFViewer>
-            </div>
-
-
-        )
+    const handeleInputQty = (e) => {
+        setInputInfo(prevState => ({
+            ...prevState, "quantity": e.target.value,
+        }))
     }
-;
+
+    const handeleInputWidth = (e) => {
+        setInputInfo(prevState => ({
+            ...prevState, "width": `${e.target.value}`,
+        }))
+    }
+
+    const handeleInputHeight = (e) => {
+        setInputInfo(prevState => ({
+            ...prevState, "height": `${e.target.value}`,
+        }))
+    }
+
+    const handeleInputSideMargin = (e) => {
+        setInputInfo(prevState => ({
+            ...prevState, "sideMargin": `${e.target.value}`,
+        }))
+    }
+
+    const handeleInputTopMargin = (e) => {
+        setInputInfo(prevState => ({
+            ...prevState, "topMargin": `${e.target.value}`,
+        }))
+    }
+
+    const handeleInputShowName = (e) => {
+        setInputInfo(prevState => ({
+            ...prevState, "showName": e.target.checked,
+        }))
+    }
+
+    const handeleInputText = (e) => {
+        setInputInfo(prevState => ({
+            ...prevState, "personalizedText": e.target.value,
+        }))
+    }
+
+    const handeleInputDefault = (e) => {
+        setIsStandard(e.target.checked);
+        setInputInfo(prevState => ({
+            ...prevState,
+            "quantity": 1,
+            "width": "42",
+            "height": "32.7",
+            "showName": true,
+            "topMargin": "0",
+            "sideMargin": "0",
+            "personalizedText": location.state.item.name,
+        }))
+    }
+
+    return (<div className="flex flex-col w-full h-screen">
+        <div className="flex w-full h-32 justify-between">
+            <div className="flex w-1/6 justify-center">
+                <ItemDetailsInput
+                    type={"checkbox"}
+                    disableInput={false}
+                    description={"Standard View"}
+                    value={isStandard}
+                    checked={isStandard}
+                    handleInput={handeleInputDefault}
+                />
+            </div>
+            {isStandard ? "" :
+                <div className="flex justify-around w-5/6 items-center ">
+                    <div className="flex-col w-2/6">
+                        <ItemDetailsInput
+                            className="flex justify-center w-full"
+                            value={inputIfo.quantity}
+                            disableInput={false}
+                            handleInput={handeleInputQty}
+                            description={'Number of Codes'}/>
+                        <ItemDetailsInput
+                            className="flex justify-center w-full"
+                            value={inputIfo.width}
+                            disableInput={false}
+                            handleInput={handeleInputWidth}
+                            description={'Width (mm)'}/>
+                        <ItemDetailsInput
+                            className="flex justify-center w-full"
+                            value={inputIfo.height}
+                            disableInput={false}
+                            handleInput={handeleInputHeight}
+                            description={'Height (mm)'}/>
+
+                    </div>
+                    <div className="flex-col justify-between w-3/6">
+                        <div className="flex justify-between">
+                            <div>
+                                <ItemDetailsInput
+                                    className="flex justify-center w-full"
+                                    value={inputIfo.sideMargin}
+                                    disableInput={false}
+                                    handleInput={handeleInputSideMargin}
+                                    description={'Side (mm)'}/>
+                                <ItemDetailsInput
+                                    className="flex justify-center w-full"
+                                    value={inputIfo.topMargin}
+                                    disableInput={false}
+                                    handleInput={handeleInputTopMargin}
+                                    description={'Top/Down (mm)'}/>
+
+
+                            </div>
+                            <div className="flex-col justify-between w-2/6">
+                                <ItemDetailsInput
+                                    type={"checkbox"}
+                                    disableInput={false}
+                                    description={"Show Name"}
+                                    value={inputIfo.showName}
+                                    checked={inputIfo.showName}
+                                    handleInput={handeleInputShowName}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex-col">
+                            <ItemDetailsInput
+                                className="flex justify-center w-full"
+                                value={inputIfo.personalizedText}
+                                disableInput={false}
+                                handleInput={handeleInputText}
+                                description={'Personilized Text'}/>
+                        </div>
+
+                    </div>
+                </div>}
+        </div>
+        <PDFViewer className="w-full flex-1">
+            <MyDocument origin={location.state.origin} item={location.state?.item} inputInfo={inputIfo}
+                        isStandard={isStandard} selection={location.state.selection}/>
+        </PDFViewer>
+    </div>)
+};
 
 export default PDFCreate
